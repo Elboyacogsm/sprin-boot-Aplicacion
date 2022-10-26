62c54d2970b3a835ee28a99d32a7e57b0fcfc7f1
Elboyacogsm <pc.enable06@gmail.com>
2022-10-25 23:08:13 -0500

    se agrega los archivos al proyecto

---
+++ src/main/java/com/pcgsmenable/app/SprintBootAppProductosApplication.java
@@ -0,0 +1,13 @@
+package com.pcgsmenable.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintBootAppProductosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintBootAppProductosApplication.class, args);
+	}
+
+}
